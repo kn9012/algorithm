c460c7a3dcce8342a53f1ef4dcf0b9d49f3067d2
김유나 <kn9012@naver.com>
2023-03-29 14:13:51 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int i = 0;
-        if (common[i + 1] - common[i] == common[common.length - 1] - common[common.length - 2] && i + 1 < common.length) {
-            answer = 2 * common[common.length - 1] - common[common.length - 2];
-        } else if (common[i + 1] / common[i] == common[common.length - 1] / common[common.length - 2] && i + 1 < common.length) {
-            answer = common[common.length - 1] * common[common.length - 1] / common[common.length - 2];
+        
+        int x = common[2] - common[1];
+        int y = common[1] - common[0];
+        
+        if(x == y) {
+            answer = common[common.length - 1] + x;
         }
-
+        else {
+            answer = common[common.length - 1] * common[2] / common[1];
+        }
+        
+        
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int common[] = {1, 2, 3, 4};
-        Solution so = new Solution();
-        System.out.println(so.solution(common));
     }
 }
