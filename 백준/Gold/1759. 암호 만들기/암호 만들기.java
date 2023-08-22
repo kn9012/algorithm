70818f0feab6905318206f1fda7424936f007436
Yuna <kn9012@naver.com>
2023-08-22 21:58:14 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,7 +1,7 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -12,7 +12,8 @@ import java.util.StringTokenizer;
  * 또한, 암호를 이루는 알파벳이 증가하는 순서로 배열되었을 때 암호들을 모두 구하는 프로그램을 작성해라.
  * 
  * [아이디어]
- * 암호 후보 문자 수를 오름차순으로 정렬한 후 조합을 사용하여 모음이 최소 1개, 자음이 최소 2개인 암호를 만든다.
+ * 조합을 이용하여 c개의 암호 후보 문자 중 l개를 골라 암호를 만든 후, 이 암호가 모음 최소 1개, 자음 최소 2개를 만족하는지 체크해준다.
+ * 이때, 모음 개수와 자음 개수를 셀 때 주의해주기!
  * 
  * 메모리 : kb
  * 실행 시간 : ms
@@ -22,7 +23,7 @@ import java.util.StringTokenizer;
  * 
  */
 public class Main {
-	static char[] vowel = {'a', 'e', 'i', 'o', 'u'};
+	static char[] vowel = {'a', 'e', 'i', 'o', 'u'}; // 모음 a, e, i, o, u
 	static char[] arr, code;
 	static int l, c;
 	static StringBuilder sb = new StringBuilder();
@@ -38,50 +39,57 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		
+		// 암호 후보 문자 배열 입력 받기
 		for (int i = 0; i < c; i++) {
 			arr[i] = st.nextToken().charAt(0);
 		}
 		
-		Arrays.sort(arr);
+		Arrays.sort(arr); // 오름차순 정렬
 		
-		comb(0, 0);
+		comb(0, 0); // 조합
 		
 		System.out.println(sb);
 	}
 	
 	static void comb(int start, int count) {
-		if (count == l) {
-			if (check()) {
+		if (count == l) { // l개를 골랐을 경우
+			if (check()) { // 조건을 만족하는지 확인
+				// 만족한다면 StringBuilder에 암호 한 글자씩 넣기
 				for (int i = 0; i < l; i++) {
 					sb.append(code[i]);
 				}
-				sb.append("\n");
+				sb.append("\n"); // 줄 띄어쓰기
 			}
-			return;
+			return; // 종료
 		}
 		
+		// 조합 부분
 		for (int i = start; i < c; i++) {
 			code[count] = arr[i];
 			comb(i + 1, count + 1);
 		}
 	}
 	
+	// 모음 최소 1개, 자음 최소 2개를 만족하는지 체크하는 함수
 	static boolean check() {
-		int vCnt = 0;
-		int cCnt = 0;
+		int vCnt = 0; // 모음 개수
+		int cCnt = 0; // 자음 개수
 		
 		for (int i = 0; i < l; i++) {
-			boolean isVowel = false;
+			boolean isVowel = false; // 모음 체크 boolean 변수
 			for (int j = 0; j < 5; j++) {
+				// 만약 모음이라면
 				if (code[i] == vowel[j]) {
-					vCnt++;
-					isVowel = true;
+					vCnt++; // count++
+					isVowel = true; // 모음인지 체크 후
 				}
 			}
-			if (!isVowel) cCnt++;
+			if (!isVowel) cCnt++; // 안쪽 for문 밖에서 count++를 해줘야 함
+			
+			// 모음 최소 1개, 자음 최소 2개를 만족한다면 true return
 			if (vCnt >= 1 && cCnt >= 2) return true;
 		}
 		
-		return false;
+		return false; // 만족하지 않는다면 false return
 	}
 }
