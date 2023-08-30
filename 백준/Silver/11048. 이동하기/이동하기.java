1d9cab9a28d5cfac9f5a78503e9a1ea789e1f3aa
Yuna <kn9012@naver.com>
2023-08-30 12:20:45 +0900

    [Silver II] Title: 이동하기, Time: 516 ms, Memory: 74328 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -6,11 +6,13 @@ import java.util.StringTokenizer;
 /**
  * 
  * [문제] 백준 11048번 이동하기
+ * 준규는 NxM 크기의 미로를 (1, 1)에서 출발하여 (N, M)로 이동하려고 할때 준규가 (r, c)에 있으면 (r+1, c), (r, c+1), (r+1, c+1)로 이동할 수 있고 각 방에 놓여있는 사탕을 가져갈 수 있다.
+ * 이때, 준규가 가져올 수 있는 사탕의 최댓값을 구하시오.
  * 
  * [아이디어]
  * 
- * 메모리 : kb
- * 실행 시간 : ms
+ * 메모리 : 72,832kb
+ * 실행 시간 : 568ms
  * 
  * @author 김유나
  * 2023-08-30
@@ -21,12 +23,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int count = 0;
+		int n = Integer.parseInt(st.nextToken()); // 행 크기
+		int m = Integer.parseInt(st.nextToken()); // 열 크기
 		
-		int arr[][] = new int[n + 1][m + 1];
+		int arr[][] = new int[n + 1][m + 1]; // 미로와 그 안에 놓인 사탕 개수
 		
+		// 입력 받기
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= m; j++) {
@@ -34,6 +36,7 @@ public class Main {
 			}
 		}
 		
+		// 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				arr[i][j] += Math.max(arr[i - 1][j], Math.max(arr[i - 1][j - 1], arr[i][j - 1]));								
