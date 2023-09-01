6d3346aca699e06575d1352aac8930fcef3092cf
Yuna <kn9012@naver.com>
2023-09-01 09:17:44 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 408 ms, Memory: 59404 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -2,44 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 
- * [문제] 백준 11729번 하노이 탑 이동 순서
- * [아이디어] 재귀를 이용한 풀이
- * 가장 작은 원판을 1, 가장 큰 원판을 n이라 할 때, 1부터 n-1까지는 도착 기둥, 즉 3번 기둥이 아닌 다른 기둥으로 옮겨야 한다.
- * n-1번째 원판까지 옮기고 나서는 도착 기둥으로 원판들을 옮겨야 한다.
- * 원판들을 옮길때마다 시간 단축을 위한 StringBuilder에 넣어주고 count를 세준다. 
- * 
- * 메모리 : kb
- * 실행 시간 : ms
- * 
- * @author 김유나
- * 2023-08-31
- * 
- */
 public class Main {
 	static int count = 0;
 	static StringBuilder sb = new StringBuilder();
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine()); // 원판 개수
+		int n = Integer.parseInt(br.readLine());
 		
-		// 원판 개수, 출발 기둥, 도착 기둥, 임시 기둥
+		// 원판 개수, 출발 장대, 도착 장대, 다른 장대
 		hanoi(n, 1, 3, 2);
+		
 		System.out.println(count);
 		System.out.println(sb);
 	}
-	
+
 	static void hanoi(int n, int from, int to, int other) {
-		// 기저조건
-		if (n == 0) {
-			return;
-		}
+		if (n == 0) return;
 		
 		count++;
-		hanoi(n - 1, from, other, to); // n-1번째 원판 다른 기둥에 옮기기
-		sb.append(from).append(" ").append(to).append("\n");
-		hanoi(n - 1, other, to, from); // 다른 기둥에 롬겨놨던 원판들 도착 기둥에 옮기기 
+		// n-1번째 원판까지는 도착 장대가 아닌 다른 장대로 일단 옮겨놔야 한다.
+		hanoi(n - 1, from, other, to);
+		sb.append(from + " " + to + "\n");
+		// n 이상의 원판부터에서는 다른 장대에서 도착 장대로 옮겨야 한다.
+		hanoi(n - 1, other, to, from);
 	}
 }
