1e9ad252267a096cd0515fd45dd2a018a4c5e7e8
Yuna <kn9012@naver.com>
2023-10-12 16:08:54 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 110 ms, Memory: 18,572 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author SSAFY
+ *
+ */
+
+public class Solution {
+	static int N, B, min, sum;
+	static int arr[], select[];
+	static boolean visited[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int TC = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= TC; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 직원 수
+			B = Integer.parseInt(st.nextToken()); // 탑의 높이
+			 
+			arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+
+			min = Integer.MAX_VALUE;
+			dfs(0, 0);
+			
+			System.out.println("#" + t + " " + (min - B));
+		}
+	}
+	
+//	static void comb(int X, int start, int count) {
+//		if (count == X) {
+//			for (int i = 0; i < select.length; i++) {
+//				sum += select[i];
+//			}
+//			
+//			if (sum >= B) {
+//				min = Math.min(min, sum);
+//				return; 
+//			}
+//		}
+//		
+//		for (int i = start; i < X; i++) {
+//			select[count] = i;
+//			comb(X, i + 1, count + 1);
+//		}
+//	}
+	
+	static void dfs(int count, int sum) {
+		if (sum >= B) {
+			min = Math.min(min, sum);
+			return;
+		}
+		if (count == N) {
+			if (sum >= B) {
+				min = Math.min(min, sum);
+			}
+			return;
+		}
+		
+		dfs(count + 1, sum + arr[count]);
+		dfs(count + 1, sum);
+		
+	}
+}
