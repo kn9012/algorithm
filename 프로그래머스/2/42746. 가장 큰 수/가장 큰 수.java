fdbad71bf94f44cacd2c11df3c03d32bfc48d78e
Yuna <kn9012@naver.com>
2024-04-14 16:31:22 +0900

    [level 2] Title: 가장 큰 수, Time: 2.22 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        String str[] = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if (str[0].equals("0")) return "0";
+        
+        for (int i = 0; i < numbers.length; i++) {
+            sb.append(str[i]);
+        }
+        
+        return sb.toString();
+    }
+}
