6aaeab8ac2c6b047206309056a6e8d8f2e14b23a
Yuna <kn9012@naver.com>
2024-06-08 14:14:41 +0900

    [Silver IV] Title: 반복수열, Time: 80 ms, Memory: 16364 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -1,46 +1,51 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-	static int A, P, nums = 0, order[];
-	static boolean isVisited[];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		A = Integer.parseInt(st.nextToken());
-		P = Integer.parseInt(st.nextToken());
-		
-		isVisited = new boolean[1000000];
-		order = new int[1000000];
-		
-		dfs(A, 0);
-        
-        System.out.println(nums);
-	}
+	import java.io.*;
+	import java.util.*;
 	
-	static void dfs(int num, int count) {
-		if (!isVisited[num]) {
-			isVisited[num] = true;
-			order[count] = num;
+	/**
+	 * 백준 2331번 반복수열
+	 * DFS 사용
+	 */	
+
+	public class Main {
+		static int A, P, nums = 0, order[];
+		static boolean isVisited[];
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			A = Integer.parseInt(st.nextToken());
+			P = Integer.parseInt(st.nextToken());
+			
+			isVisited = new boolean[1000000];
+			order = new int[1000000];
+			
+			dfs(A, 0);
+	        
+	        System.out.println(nums);
 		}
 		
-		else {
-			for (int i = 0; i <= count; i++) {
-				if (order[i] == num) {
-					nums = i;
-					break;
-				}
+		static void dfs(int num, int count) {
+			if (!isVisited[num]) {
+				isVisited[num] = true;
+				order[count] = num;
 			}
-			return;
+			
+			else {
+				for (int i = 0; i <= count; i++) {
+					if (order[i] == num) {
+						nums = i;
+						break;
+					}
+				}
+				return;
+			}
+			
+			int sum = 0;
+			while (num > 0) {
+				sum += Math.pow(num % 10, P);
+				num /= 10;
+			}
+			
+			dfs(sum, count + 1);
 		}
-		
-		int sum = 0;
-		while (num > 0) {
-			sum += Math.pow(num % 10, P);
-			num /= 10;
-		}
-		
-		dfs((int)sum, count + 1);
 	}
-}
