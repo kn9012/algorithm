fc1ebef1a74136ec64d169d6129ad9582ee16b0b
Yuna <kn9012@naver.com>
2024-06-19 20:49:24 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -12,21 +12,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
-        
-        if (arr.length == 1) return arr[0];
-        
-        int g = gcd(arr[0], arr[1]);
-        answer = arr[0] * arr[1] / g;
-        
-        if (arr.length > 2) {
-            for (int i = 2; i < arr.length; i++) {
-                g = gcd(answer, arr[i]);
-                answer = answer * arr[i] / g;
-            }
+        int answer = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer = answer * arr[i] / gcd(answer, arr[i]);
         }
                 
-        
         return answer;
     }
     
