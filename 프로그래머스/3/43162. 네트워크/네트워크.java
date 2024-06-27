1978d8b8e2926dfaf3654aad9f29d2f9716626ca
Yuna <kn9012@naver.com>
2024-06-27 13:26:00 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,14 +1,21 @@
+/**
+ * 프로그래머스 네트워크
+ *
+ * 아이디어 - DFS
+ * computers[i][i]는 0으로 변경하고 DFS로 연결된 컴퓨터 방문
+*/
+
 class Solution {
     boolean isVisited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        isVisited = new boolean[computers.length];
+        isVisited = new boolean[n];
         
-        for (int i = 0; i < computers.length; i++) {
+        for (int i = 0; i < n; i++) {
             computers[i][i] = 0;
         }
         
-        for (int i = 0; i < computers.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
                 answer++;
                 isVisited[i] = true;
