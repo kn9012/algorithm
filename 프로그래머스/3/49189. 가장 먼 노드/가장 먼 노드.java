69a2a7266ad0f1ad0ed57ae610cb2fcbf3539c41
Yuna <kn9012@naver.com>
2024-07-03 17:09:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 1515.69 ms, Memory: 630 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,15 +2,15 @@
  * 프로그래머스 가장 먼 노드
  * 
  * 아이디어 - 최단 거리를 구해야하므로 BFS!
- * 
+ * 인접 배열을 만들어서 사용했는데 int형으로 하면 테스트 8, 9번에서 메모리 초과가 난다
+ * boolean 타입으로 선언하면 안 남
 */
 
 import java.util.*;
 
 class Solution {
-    static int min, value[];
     static boolean isVisited[], arr[][];
-    //static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer> list = new ArrayList<>();
     public class Node {
         int index, weight;
         
@@ -25,7 +25,6 @@ class Solution {
         
         arr = new boolean[n][n];
         isVisited = new boolean[n];
-        value = new int[n];
         
         for (int i = 0; i < edge.length; i++) {
             int num1 = edge[i][0] - 1;
@@ -35,19 +34,12 @@ class Solution {
             arr[num2][num1] = true;
         }
         
-        min = Integer.MAX_VALUE;
         bfs(0);
         
-        //Collections.sort(list);
-        Arrays.sort(value);
-        
-        int max = value[value.length - 1];
-        //int max = list.get(list.size() - 1);
-        // for (int i = list.size() - 2; i > 0; i--) {
-        //     if (max == list.get(i)) answer++;
-        // }
-        for (int i = value.length - 2; i > 0; i--) {
-            if (max == value[i]) answer++;
+        Collections.sort(list);
+        int max = list.get(list.size() - 1);
+        for (int i = list.size() - 2; i > 0; i--) {
+            if (max == list.get(i)) answer++;
         }
         return answer;
     }
@@ -56,19 +48,17 @@ class Solution {
         Queue<Node> queue = new ArrayDeque<>();
         queue.add(new Node(start, 0));
         isVisited[start] = true;
-        int count = 0;
         
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             int index = cur.index;
             int weight = cur.weight;
             
-            for (int i = 0; i < arr.length; i++) {
+            for (int i = 1; i < arr.length; i++) {
                 if (!isVisited[i] && arr[index][i]) {
                     queue.add(new Node(i, weight + 1));
                     isVisited[i] = true;
-                    //list.add(weight + 1);
-                    value[i] = weight + 1;
+                    list.add(weight + 1);
                 }
             }
         }
