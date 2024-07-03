8046d439cfdb1bc4b61853d2df4609905d07c327
Yuna <kn9012@naver.com>
2024-07-03 15:57:39 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.26 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -4,6 +4,7 @@
  * 아이디어 - DFS
  * visit 배열을 이용해서 이미 연 상자는 체크
  * ArrayList에 상자의 수를 입력해서 오름차순 정렬한 후 가장 앞의 두 값을 곱하면 최고 점수
+ * 주의할 점은 그룹이 1개일 경우에는 점수 0점 처리해주기
 */
 
 import java.util.*;
@@ -12,7 +13,6 @@ class Solution {
     boolean isVisited[];
     ArrayList<Integer> list = new ArrayList<>();
     public int solution(int[] cards) {
-        int answer = 0;
         isVisited = new boolean[cards.length];
         
         for (int i = 0; i < cards.length; i++) {
@@ -22,9 +22,8 @@ class Solution {
         int size = list.size();
         Collections.sort(list);
         if (size != 1) {
-            answer = list.get(size - 1) * list.get(size - 2);
-        } else answer = 0;
-        return answer;
+            return list.get(size - 1) * list.get(size - 2);
+        } else return 0;
     }
     
     public void dfs(int index, int count, int[] cards) {
@@ -33,8 +32,7 @@ class Solution {
             return;
         }
         
-        int value = cards[index] - 1;
         isVisited[index] = true; 
-        dfs(value, count + 1, cards);
+        dfs(cards[index] - 1, count + 1, cards);
     }
 }
