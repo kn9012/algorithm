78c46410026c685ee778795fc25187e3c6ab07c2
Yuna <kn9012@naver.com>
2024-07-05 12:03:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 1.21 ms
+메모리: 79 MB, 시간: 1.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 04일 21:39:41
+2024년 07월 05일 12:03:10
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,30 +1,34 @@
 /**
  * 프로그래머스 디스크 컨트롤러
- *
- * 아이디어 - PriorityQueue 사용
+ * 
+ * 1. jobs의 요청 시점이 오름차순대로 들어오지 않을수도 있기 때문에 오름차순 정렬
+ * 2. PriorityQueue 사용해서 소요 시간 오름차순 정렬
+ * 3. 종료 시간 (end), 현재 인덱스 (index), 총 걸린 시간의 합(answer), 현재 진행한 작업 개수(count)
  */
 
 import java.util.*;
-
-class Solution {    
-    int nums, min = Integer.MAX_VALUE;
+class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
+        
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
         
-        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        PriorityQueue<int []> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         
-        int time = 0;
-        int index = 0;
         int count = 0;
+        int index = 0;
+        int end = 0;
         
         while (count < jobs.length) {
-            while (index < jobs.length && jobs[index][0] <= time) q.add(jobs[index++]);
-            if (q.isEmpty()) time = jobs[index][0];
+            // 요청 시점이 end보다 작거나 같은 것들 다 넣기
+            while (index < jobs.length && jobs[index][0] <= end) pq.add(jobs[index++]);
+            
+            // 큐가 비어있으면 end를 요청 시간으로 초기화
+            if (pq.isEmpty()) end = jobs[index][0];
             else {
-                int[] cur = q.poll();
-                answer += cur[1] + time - cur[0];
-                time += cur[1];
+                int[] cur = pq.poll();
+                answer += cur[1] + end - cur[0];
+                end += cur[1];
                 count++;
             }
         }
