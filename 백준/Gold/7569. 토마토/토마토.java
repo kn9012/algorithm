1ff8b6c503c3db84fea1060cccec04e4e2fe5f8f
Yuna <kn9012@naver.com>
2024-07-15 17:14:31 +0900

    [Gold V] Title: 토마토, Time: 572 ms, Memory: 109356 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,81 +1,112 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 7569번 토마토
+ * 
+ * 3차원 배열과 BFS 사용
+ * 이때, 익어있는 토마토를 ArrayList에 넣고 동시에 진행되도록 함
+ * 
+ * 1 : 익은 토마토
+ * 0 : 익지 않은 토마토
+ * -1 : 토마토가 들어있지 않은 칸
+ * 모든 토마토가 익기까지 며칠이 걸리는지 출력
+ * 이때, 모든 토마토가 익어 있으면 0, 모두 익지 못하면 -1
+ */
 
 public class Main {
-	static int M, N, H, map[][][];
-	// 앞, 뒤, 왼쪽, 오른쪽, 앞, 뒤
-	static int X[] = {-1, 0, 1, 0, 0, 0}, Y[] = {0, 1, 0, -1, 0, 0}, Z[] = {0, 0, 0, 0, 1, -1};
-	static Queue<Point> queue;
-
 	static class Point {
-		int x, y, z;
-
-		public Point(int z, int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}		
+		int m, n, h, count;
+		
+		public Point (int m, int n, int h, int count) {
+			this.m = m;
+			this.n = n;
+			this.h = h;
+			this.count = count;
+		}
 	}
-
+	
+	static int N, M, H, box[][][], answer = 0, notRipeCount = 0;
+	static boolean isSelected[][][];
+	static int deltas[][] = {{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
+	static ArrayList<Point> tomato = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
-		Scanner s = new Scanner(System.in);
-		M = s.nextInt();
-		N = s.nextInt();
-		H = s.nextInt();
-
-		map = new int[H][N][M];
-		queue = new ArrayDeque<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 상자의 가로 칸 수
+		M = Integer.parseInt(st.nextToken()); // 상자의 세로 칸 수
+		H = Integer.parseInt(st.nextToken()); // 상자의 수
+		
+		box = new int[M][N][H];
+		isSelected = new boolean[M][N][H];
 		
 		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < M; k++) {
-					map[i][j][k] = s.nextInt();
+			for (int j = 0; j < M; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < N; k++) {
+					box[j][k][i] = Integer.parseInt(st.nextToken());
 					
-					if (map[i][j][k] == 1) {
-						queue.add(new Point(i, j, k));
+					if (box[j][k][i] == 1) tomato.add(new Point(j, k, i, 0));
+					else if (box[j][k][i] == 0) notRipeCount++;
+				}
+			}
+		}
+		
+		if (notRipeCount == 0) {
+			System.out.println(0);
+			return;
+		}
+		
+		bfs();
+		
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < M; j++) {
+				for (int k = 0; k < N; k++) {
+					if (!isSelected[j][k][i] && box[j][k][i] == 0) {
+						System.out.println(-1);
+						return;
 					}
 				}
 			}
 		}
-
-		System.out.println(bfs());
+		
+		System.out.println(answer);
+		
 	}
-
-	static int bfs() {
+	
+	public static void bfs() {
+		Queue<Point> queue = new ArrayDeque<>();
+		
+		for (Point point : tomato) {
+			queue.add(point);
+			isSelected[point.m][point.n][point.h] = true;
+		}
+		
 		while (!queue.isEmpty()) {
 			Point cur = queue.poll();
-			// 위, 아래, 왼쪽, 오른쪽, 앞, 뒤
+			
+			int X = cur.m;
+			int Y = cur.n;
+			int Z = cur.h;
+			int count = cur.count;
+			answer = count;
+			
+			
 			for (int i = 0; i < 6; i++) {
-				int dx = cur.x + X[i];
-				int dy = cur.y + Y[i];
-				int dz = cur.z + Z[i];
+				int dx = X + deltas[i][0];
+				int dy = Y + deltas[i][1];
+				int dz = Z + deltas[i][2];
 				
-				if (dx >= 0 && dy >= 0 && dz >= 0 && dx < N && dy < M && dz < H) {
-					if (map[dz][dx][dy] == 0) {
-						queue.add(new Point(dz, dx, dy));
-						map[dz][dx][dy] = map[cur.z][cur.x][cur.y] + 1;
-					}
-				}				
+				if (dx >= M || dy >= N || dz >= H || dx < 0 || dy < 0 || dz < 0 || isSelected[dx][dy][dz] || box[dx][dy][dz] != 0) continue;
+				
+				queue.add(new Point(dx, dy, dz, count + 1));
+				isSelected[dx][dy][dz] = true;
 			}
 		}
-		
-		int result = Integer.MIN_VALUE;
-		
-		// while문이 종료했는데 익지 않은 토마토가 있다면 
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < M; k++) {
-					if (map[i][j][k] == 0) return -1;
-					
-					result = Math.max(result, map[i][j][k]);
-				}
-			}
-		}
-		
-		if (result == 1) return 0;
-		else return result - 1;
 	}
 }
