876ece756682903e34e12c77bb4b1bbb0aa411f3
Yuna <kn9012@naver.com>
2024-09-06 10:54:08 +0900

    [Silver I] Title: 지름길, Time: 68 ms, Memory: 11676 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -5,38 +5,37 @@ import java.util.StringTokenizer;
 
 /**
  * 백준 1446번 지름길
+ * - dp로 풀이
+ * - 고속도로를 역주행할 수는 없음
  * 
- * 
- *
+ * 메모리 : kb 시간 : ms
  */
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		int N = Integer.parseInt(st.nextToken()); // 지름길 개수
 		int D = Integer.parseInt(st.nextToken()); // 고속도로 길이
-		
-		int arr[][] = new int[N][3];
+		int shortcut[][] = new int[N][3];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			for (int j = 0; j < 3; j++) {
-				// 지름길 시작 위치, 도착 위치, 지름길 길이
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				shortcut[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int dp[] = new int[10001];
+		int dp[] = new int[D + 1];
 		Arrays.fill(dp, Integer.MAX_VALUE);
 		dp[0] = 0;
-
 		
-		for (int i = 1; i < dp.length; i++) {
+		for (int i = 1; i <= D; i++) {
 			for (int j = 0; j < N; j++) {
-				if (arr[j][1] == i) {
-					dp[i] = Math.min(dp[i], Math.min(dp[i - 1] + 1, dp[arr[j][0]] + arr[j][2]));
+				if (shortcut[j][1] == i) {
+					dp[i] = Math.min(dp[i], Math.min(dp[i - 1] + 1, dp[shortcut[j][0]] + shortcut[j][2]));
 				} else {
 					dp[i] = Math.min(dp[i], dp[i - 1] + 1);
 				}
