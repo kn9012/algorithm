1285b568de02978f49a9027436ba88c106e82a33
Yuna <kn9012@naver.com>
2024-09-15 15:27:58 +0900

    [level 3] Title: 섬 연결하기, Time: 0.75 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,17 +1,26 @@
 import java.util.*;
 
+/**
+ * 프로그래머스 섬 연결하기
+ * - 최소 신장 트리(MST), 크루스칼 union find 사용
+ */
+
 class Solution {
     static int parent[];
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        Arrays.sort(costs, (int [] c1, int [] c2) -> c1[2]-c2[2]);
         
+        // 비용을 기준으로 오름차순 정렬
+        Arrays.sort(costs, (int[] c1, int[] c2) -> c1[2] - c2[2]);
+        
+        // 부모 저장할 배열
         parent = new int[n];
+        
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
-        for (int edge[] : costs) {
+        for (int[] edge : costs) {
             int from = edge[0];
             int to = edge[1];
             int cost = edge[2];
@@ -28,8 +37,8 @@ class Solution {
         return answer;
     }
     
-    private int findParent(int node) {
+    public int findParent(int node) {
         if (parent[node] == node) return node;
-        return parent[node] = findParent(parent[node]);
+        return findParent(parent[node]);
     }
 }
