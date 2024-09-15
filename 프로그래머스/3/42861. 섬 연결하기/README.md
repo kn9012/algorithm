a0bf18336f65bc4d3076382e3da37f30fc79351b
Yuna <kn9012@naver.com>
2024-09-15 15:30:25 +0900

    [level 3] Title: 섬 연결하기, Time: 0.94 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
 
 ### 성능 요약
 
-메모리: 67.9 MB, 시간: 0.75 ms
+메모리: 73.1 MB, 시간: 0.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 15일 15:27:55
+2024년 09월 15일 15:30:23
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -17,7 +17,7 @@ class Solution {
         parent = new int[n];
         
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
+            parent[i] = i; // 처음엔 부모 노드를 본인으로 초기화
         }
         
         for (int[] edge : costs) {
@@ -25,12 +25,17 @@ class Solution {
             int to = edge[1];
             int cost = edge[2];
             
+            // 부모 찾기
             int fromParent = findParent(from);
             int toParent = findParent(to);
             
+            // from 부모와 to 부모가 같을 경우 같은 무리에 있는 것이므로 연결하지 않음
             if (fromParent == toParent) continue;
             
+            // 부모가 다를 경우 연결
             answer += cost;
+            
+            // to의 부모를 from의 부모로 변경
             parent[toParent] = fromParent;
         }
         
