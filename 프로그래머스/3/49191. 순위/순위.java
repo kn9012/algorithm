99669119b614c2e753ef9cb4ae22846623185a0b
Yuna <kn9012@naver.com>
2024-09-17 11:10:52 +0900

    [level 3] Title: 순위, Time: 58.23 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            boolean winVisited[] = new boolean[n + 1];
+            boolean loseVisited[] = new boolean[n + 1];
+            
+            int count = winDFS(i, results, winVisited) + loseDFS(i, results, loseVisited);
+            
+            if (count == n - 1) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public int winDFS(int num, int[][] results, boolean visited[]) {
+        int count = 0;
+        visited[num] = true;
+        
+        for (int i = 0; i < results.length; i++) {
+            if (results[i][0] == num && !visited[results[i][1]]) {
+                count = count + winDFS(results[i][1], results, visited) + 1;
+            }
+        }
+        
+        return count;
+    }
+    
+    public int loseDFS(int num, int[][] results, boolean visited[]) {
+        int count = 0;
+        visited[num] = true;
+        
+        for (int i = 0; i < results.length; i++) {
+            if (results[i][1] == num && !visited[results[i][0]]) {
+                count = count + loseDFS(results[i][0], results, visited) + 1;
+            }
+        }
+        
+        return count;
+    }
+}
