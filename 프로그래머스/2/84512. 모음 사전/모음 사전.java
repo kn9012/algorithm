bb88685ddb6ac0ff12440d7e07ed2baec50f856d
Yuna <kn9012@naver.com>
2024-09-18 16:09:04 +0900

    [level 2] Title: 모음 사전, Time: 45.60 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,29 @@
+/**
+ * 프로그래머스 모음 사전
+ * - 완탐..
+ */
+
+import java.util.*;
+
+class Solution {
+    static List<String> words = new ArrayList<>();
+    public int solution(String word) {
+        int answer = 0;
+        
+        createWord("");
+        
+        return words.indexOf(word);
+    }
+    
+    public void createWord(String word) {
+        if (word.length() > 5) {
+            return;
+        }
+        
+        words.add(word);
+        
+        for (int i = 0; i < 5; i++) {
+            createWord(word + "AEIOU".charAt(i));
+        }
+    }
+}
