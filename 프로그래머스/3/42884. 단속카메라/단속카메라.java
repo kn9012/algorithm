d7440ff7696945f4adbe3a78596ba6de86e1bee3
Yuna <kn9012@naver.com>
2024-09-19 15:36:27 +0900

    [level 3] Title: 단속카메라, Time: 8.01 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,6 +1,6 @@
 /**
  * 프로그래머스 단속카메라
- * 그리디
+ * - 그리디 사용
  */
 
 import java.util.*;
@@ -10,10 +10,12 @@ class Solution {
         // 진출 지점을 기준으로 오름차순 정렬
         Arrays.sort(routes, (a, b) -> a[1] - b[1]);
         
+        // 첫 카메라는 첫 차량의 진출 지점
         int max = routes[0][1];
         int answer = 1;
         
         for (int[] route : routes) {
+            // 다음 차량의 진입 지점이 카메라 위치보다 크다면 갱신하고 카메라 설치
             if (max < route[0]) {
                 max = route[1];
                 answer++;
