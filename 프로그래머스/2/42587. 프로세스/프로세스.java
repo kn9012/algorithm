4712865eb9972a44de2b973a6d789705ccf4466d
Yuna <kn9012@naver.com>
2024-09-20 12:57:13 +0900

    [level 2] Title: 프로세스, Time: 1.22 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,9 +1,15 @@
+/**
+ * 프로그래머스 프로세스
+ * - Queue 사용
+ */
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
+        // 실행 대기 큐
         Queue<int []> wait = new ArrayDeque<>();
         
         for (int i = 0; i < priorities.length; i++) {
@@ -11,7 +17,7 @@ class Solution {
         }
         
         while (!wait.isEmpty()) {
-            int[] first = wait.peek();
+            int[] first = wait.poll(); // 가장 앞의 값
         
             int max = 0;
             int maxIndex = -1;
@@ -23,13 +29,13 @@ class Solution {
                 }
             }
             
-            if (max > first[0]) wait.add(wait.poll());
+            // 우선순위가 높은 프로세스가 있으면 방금 꺼낸거 다시 넣기
+            if (max > first[0]) wait.add(first);
             else {
                 answer++;
-                int execution[] = wait.poll();
-                priorities[maxIndex] = 0;
+                priorities[maxIndex] = 0; // 꺼낸 값 초기화
                 
-                if (execution[1] == location) break;
+                if (first[1] == location) break;
             }
         }
         
