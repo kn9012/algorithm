129e6169a249a7afea44081f5dbd880f6772bdcf
Yuna <kn9012@naver.com>
2024-09-24 18:58:00 +0900

    [level 3] Title: 최고의 집합, Time: 0.46 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int n, int s) {
-        List<Integer> list = new ArrayList<>();
-        int count = 0;
-                
-        while (n > 0 && s / n > 0) {
+    public int[] solution(int n, int s) {
+        int[] answer = new int[n];
+        if (n > s) return new int[] {-1};
+        else {
+            int count = 0;
+            
+            while (n > 0 && s / n > 0) {
                 int num = s / n--;
-                list.add(num);
+                answer[count++] = num;
                 s -= num;
+            }
         }
         
-        if (list.size() == 0) list.add(-1);
-        
-        return list;
+        return answer;
     }
 }
