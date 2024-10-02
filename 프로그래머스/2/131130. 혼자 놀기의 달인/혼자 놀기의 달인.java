2674dc79a7d59260ffde2e49354cf339d319ea6d
Yuna <kn9012@naver.com>
2024-10-02 14:50:14 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.78 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,38 +1,35 @@
 /**
  * 프로그래머스 혼자 놀기의 달인
- *
- * 아이디어 - DFS
- * visit 배열을 이용해서 이미 연 상자는 체크
- * ArrayList에 상자의 수를 입력해서 오름차순 정렬한 후 가장 앞의 두 값을 곱하면 최고 점수
- * 주의할 점은 그룹이 1개일 경우에는 점수 0점 처리해주기
-*/
+ * - DFS로 최댓값 구하기
+ */
 
 import java.util.*;
 
 class Solution {
+    List<Integer> list = new ArrayList<>();
     boolean isVisited[];
-    ArrayList<Integer> list = new ArrayList<>();
+    
     public int solution(int[] cards) {
         isVisited = new boolean[cards.length];
         
         for (int i = 0; i < cards.length; i++) {
-            if (!isVisited[i]) dfs(i, 0, cards);
+            if (!isVisited[i]) dfs(0, i, cards);
         }
         
-        int size = list.size();
-        Collections.sort(list);
-        if (size != 1) {
-            return list.get(size - 1) * list.get(size - 2);
-        } else return 0;
+        Collections.sort(list, (o1, o2) -> o2 - o1);
+        
+        if (list.size() == 1) return 0;
+        else return list.get(0) * list.get(1);
     }
     
-    public void dfs(int index, int count, int[] cards) {
-        if (isVisited[index]) {
+    public void dfs(int count, int num, int[] cards) {
+        if (isVisited[num]) {
             list.add(count);
             return;
         }
         
-        isVisited[index] = true; 
-        dfs(cards[index] - 1, count + 1, cards);
+        isVisited[num] = true;
+        dfs(count + 1, cards[num] - 1, cards);
+
     }
 }
