16841c4bc8fcf13c3ac61a75da438d4330dfe573
Yuna <kn9012@naver.com>
2024-10-16 17:08:41 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 236 ms, Memory: 36964 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -2,31 +2,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 백준 20922번 겹치는 건 싫어
- * - 투 포인터 사용?
- * 
- * 메모리 : kb 시간 : ms
- */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
 		int arr[] = new int[N];
-		int num[] = new int[100001];
-		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int num[] = new int[100001];
 		int max = 0;
 		int start = 0;
-		int end = 0;
+		int end = 1;
+		
+		num[arr[start]]++;
 		
 		while (end < N) {
 			while (end < N && num[arr[end]] < K) {
