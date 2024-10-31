5f1ec3f74b48858887a768888526e19f829a051e
Yuna <kn9012@naver.com>
2024-10-31 13:29:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -6,18 +6,8 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Time {
-		int num, count;
-		
-		Time(int num, int count) {
-			this.num = num;
-			this.count = count;
-		}
-	}
-	
-	static int N, K, answer;
-	static boolean isVisited[];
-	
+	static int N, K, min = Integer.MAX_VALUE;
+	static boolean visit[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,38 +15,38 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		isVisited = new boolean[100001];
+		visit = new boolean[100001];
 		
 		bfs();
 		
-		System.out.println(answer);
+		System.out.println(min);
 	}
 	
 	public static void bfs() {
-		Queue<Time> queue = new ArrayDeque<>();
-		queue.add(new Time(N, 0));
-		isVisited[N] = true;
+		Queue<int []> queue = new ArrayDeque<>();
+		queue.add(new int[] {N, 0});
+		visit[N] = true;
 		
 		while (!queue.isEmpty()) {
-			Time cur = queue.poll();
-			if (cur.num == K) {
-				answer = cur.count;
-				break;
+			int cur[] = queue.poll();
+			int loc = cur[0];
+			int count = cur[1];
+			
+			if (loc == K) min = Math.min(min, count);
+			
+			if (loc * 2 <= 100000 && !visit[loc * 2]) {
+				queue.add(new int[] {loc * 2, count});
+				visit[loc * 2] = true;
+			}
+			if (loc - 1 >= 0 && !visit[loc - 1]) {
+				queue.add(new int [] {loc - 1, count + 1});
+				visit[loc - 1] = true;
+			}
+			if (loc + 1 <= 100000 && !visit[loc + 1]) {
+				queue.add(new int [] {loc + 1, count + 1});
+				visit[loc + 1] = true;
 			}
 			
-			if (cur.num * 2 <= 100000 && !isVisited[cur.num * 2]) {
-				queue.add(new Time(cur.num * 2, cur.count));
-				isVisited[cur.num * 2] = true;
-			}
-			if (cur.num - 1 >= 0 && !isVisited[cur.num - 1]) {
-				queue.add(new Time(cur.num - 1, cur.count + 1));
-				isVisited[cur.num - 1] = true;
-			}
-			if (cur.num + 1 <= 100000 && !isVisited[cur.num + 1]) {
-				queue.add(new Time(cur.num + 1, cur.count + 1));
-				isVisited[cur.num + 1] = true;
-			}
-
 		}
 	}
 }
