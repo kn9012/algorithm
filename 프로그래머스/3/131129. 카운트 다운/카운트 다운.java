194c1084d908de6efbca2d745475775ad8896e7f
Yuna <kn9012@naver.com>
2024-12-29 22:26:26 +0900

    [level 3] Title: 카운트 다운, Time: 42.50 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -3,51 +3,50 @@ class Solution {
         int[] answer = new int[2];
         int[][] dp = new int[target + 1][2];
         
-        for(int i = 0; i <= target; i++){
+        for (int i = 0; i <= target; i++) {
             dp[i][0] = Integer.MAX_VALUE;
             dp[i][1] = 0;
         }
         
         dp[0][0] = 0;
         
-        for(int i = 1; i <= target; i++){
-            for(int j = 1; j <= 20; j++){
-                if(i - 50 >= 0){
-                    if(dp[i][0] > dp[i - 50][0] + 1){
+        for (int i = 1; i <= target; i++) {
+            for (int j = 1; j <= 20; j++) {
+                // 불
+                if (i >= 50) {
+                    if (dp[i][0] > dp[i - 50][0] + 1) {
                         dp[i][0] = dp[i - 50][0] + 1;
                         dp[i][1] = dp[i - 50][1] + 1;
                     }
-                    else if(dp[i][0] == dp[i - 50][0] + 1){
-                        dp[i][1] = Math.max(dp[i][1], dp[i - 50][1] + 1); 
+                    else if (dp[i][0] == dp[i - 50][0] + 1) {
+                        dp[i][1] = Math.max(dp[i][1], dp[i - 50][1] + 1);
                     }
-                    
                 }
                 
                 // single
-                if(i - j >= 0){
-                    if(dp[i][0] > dp[i - j][0] + 1){
+                if (i >= j) {
+                    if (dp[i][0] > dp[i - j][0] + 1) {
                         dp[i][0] = dp[i - j][0] + 1;
                         dp[i][1] = dp[i - j][1] + 1;
                     }
-                    else if(dp[i][0] == dp[i - j][0] + 1){
-                        dp[i][1] = Math.max(dp[i][1], dp[i - j][1] + 1); 
+                    else if (dp[i][0] == dp[i - j][0] + 1) {
+                        dp[i][1] = Math.max(dp[i][1], dp[i - j][1] + 1);
                     }
                 }
                 
                 // double
-                if(i - 2 * j >= 0){
-                    if(dp[i][0] > dp[i - 2 * j][0] + 1){
-                        dp[i][0] = dp[i - 2 * j][0] + 1;
-                        dp[i][1] = dp[i - 2 * j][1];
+                if (i >= j * 2) {
+                    if (dp[i][0] > dp[i - j * 2][0] + 1) {
+                        dp[i][0] = dp[i - j * 2][0] + 1;
+                        dp[i][1] = dp[i - j * 2][1];
                     }
-                    
                 }
                 
                 // triple
-                if(i - 3 * j >= 0){
-                    if(dp[i][0] > dp[i - 3 * j][0] + 1){
-                        dp[i][0] = dp[i - 3 * j][0] + 1; 
-                        dp[i][1] = dp[i - 3 * j][1];
+                if (i >= j * 3) {
+                    if (dp[i][0] > dp[i - j * 3][0] + 1) {
+                        dp[i][0] = dp[i - j * 3][0] + 1;
+                        dp[i][1] = dp[i - j * 3][1];
                     }
                 }
             }
