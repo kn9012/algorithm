eeaa6b0be57b0a3a080cc1b80a62aff19be2d4ba
Yuna <kn9012@naver.com>
2025-01-05 10:24:36 +0900

    [level 2] Title: 더 맵게, Time: 1610.44 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,29 +1,24 @@
-/**
- * 프로그래머스 더 맵게
- * PriorityQueue 사용
- */
-
 import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        
-        for (int food : scoville) {
-            queue.add(food);
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        for (int i = 0; i < scoville.length; i++) {
+            queue.add(scoville[i]);
         }
         
         while (queue.peek() < K) {
-            // 음식이 하나밖에 없는데 K보다 작다면 -1
             if (queue.size() == 1) {
                 answer = -1;
                 break;
             }
-            // 가장 맵지 않은 음식이랑 두번째로 맵지 않은 음식 섞어서 새로 추가하기
-            int newFood = queue.poll() + (queue.poll() * 2);            
-            queue.add(newFood);
+            
+            int min = queue.poll();
+            int nextMin = queue.poll();
+            
+            queue.add(min + (nextMin * 2));
             
             answer++;
         }
