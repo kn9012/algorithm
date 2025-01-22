da25b86c1e5d1b7b27b5368e5fef7124b2c9ab50
Yuna <kn9012@naver.com>
2025-01-22 18:07:19 +0900

    [level 3] Title: 징검다리 건너기, Time: 35.21 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,42 @@
+/**
+ * 프로그래머스 징검다리 건너기
+ * - 한 번에 건너뛸 수 있는 디딤돌의 최대 칸수 : k -> 0이 k-1개 연속으로 나와야 함
+ * - 이분 탐색? 슬라이딩 윈도우?
+ */
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for (int stone : stones) {
+            max = Math.max(max, stone);
+        }
+        
+        int left = 1;
+        int right = max;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            int zeroCount = 0;
+            boolean isZero = false;
+            
+            for (int stone : stones) {
+                if (stone - mid <= 0) zeroCount++;
+                else zeroCount = 0;
+                
+                if (zeroCount >= k) isZero = true;
+            }
+            
+            if (!isZero) {
+                answer = Math.max(answer, mid);
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        
+        return answer + 1;
+    }
+}
