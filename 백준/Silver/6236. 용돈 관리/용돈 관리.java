4a0acfac65c49a7544b741e8ce69c0c1e208990c
Yuna <kn9012@naver.com>
2025-02-04 19:19:54 +0900

    [Silver I] Title: 용돈 관리, Time: 200 ms, Memory: 20792 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 백준 6236번 용돈 관리
+ * - 매개변수 이분탐색
+ */
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int money[] = new int[N];
+		int left = 1;
+		int right = 1;
+		
+		for (int i = 0; i < N; i++) {
+			money[i] = Integer.parseInt(br.readLine());
+			left = Math.max(left, money[i]);
+			right += money[i];
+		}
+
+		int mid = 0;
+		
+		while (left <= right) {
+			mid = (left + right) / 2;
+			int count = 1;
+			int wallet = 0;
+			
+			for (int i = 0; i < N; i++) {
+				wallet += money[i];
+				
+				if (wallet > mid) {
+					count++;
+					wallet = money[i];
+				}
+			}
+            
+			if (count <= M) {
+				right = mid - 1;
+			} else if (count > M) {
+				left = mid + 1;
+			}
+		}
+		
+		System.out.println(left);
+	}
+}
