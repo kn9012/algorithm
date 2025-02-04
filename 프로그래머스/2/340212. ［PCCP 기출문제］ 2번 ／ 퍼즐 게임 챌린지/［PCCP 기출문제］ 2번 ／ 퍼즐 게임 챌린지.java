d8417f361256c55cc99cd3aec1c7c72d4f1a4ebb
Yuna <kn9012@naver.com>
2025-02-04 20:25:32 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 56.39 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,46 +1,45 @@
 /**
  * 프로그래머스 퍼즐 게임 챌린지
- * - 값의 이분 탐색?
+ * - 이분 탐색
  */
-
-import java.util.*;
 
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int answer = 0;
         
-        for (int i = 0; i < diffs.length; i++) {
-            min = Math.min(min, diffs[i]);
-            max = Math.max(max, diffs[i]);
+        int len = diffs.length;
+        int left = 0, right = 0;
+        for (int i = 0; i < len; i++) {
+            left = Math.min(left, diffs[i]);
+            right = Math.max(right, diffs[i]);
         }
         
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        
-        while (min <= max) {
-            long time = 0;
-            long preTime = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long totalTime = 0;
+            int prevTime = 0;
             
-            int mid = (min + max) / 2;
+            boolean isExceed = false;
             
-            for (int i = 0; i < diffs.length; i++) {
-                if (mid >= diffs[i]) {
-                    time += times[i];
-                    preTime = times[i];
+            for (int i = 0; i < len; i++) {
+                if (diffs[i] <= mid) {
+                    totalTime += times[i];
+                    prevTime = times[i];
                 } else {
-                    time += (diffs[i] - mid) * (times[i] + preTime) + times[i];
-                    preTime = times[i];
+                    totalTime += (diffs[i] - mid) * (times[i] + prevTime) + times[i];
+                    prevTime = times[i];
                 }
             }
-                             
-            if (time > limit) {
-                min = mid + 1;
+            
+            if (totalTime > limit) {
+                answer = mid;
+                left = mid + 1;
             } else {
-                queue.add(mid);
-                max = mid - 1;
+                right = mid - 1;
             }
         }
         
-        return queue.poll();
+        
+        return answer + 1;
     }
 }
