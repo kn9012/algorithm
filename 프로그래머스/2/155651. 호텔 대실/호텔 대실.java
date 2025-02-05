2aed92cac85a675deb5887c133f1e60bac42c1d7
Yuna <kn9012@naver.com>
2025-02-05 12:26:36 +0900

    [level 2] Title: 호텔 대실, Time: 6.53 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,19 +1,20 @@
-import java.util.*;
-
 /**
  * 프로그래머스 호텔 대실
- * - 시작 시간을 기준으로 배열 오름차순한 뒤, 우선순위 큐에 끝나는 시간을 넣으면서 비교
+ * - 시작 시간을 기준으로 오름차순 정렬
+ * - 
  */
+
+import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        
         Arrays.sort(book_time, (o1, o2) -> o1[0].compareTo(o2[0]));
         
-        for (String times[] : book_time) {
-            int start = changeMin(times[0]);
-            int end = changeMin(times[1]) + 10;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for (String books[] : book_time) {
+            int start = changeToMinute(books[0]);
+            int end = changeToMinute(books[1]) + 10;
             
             if (!queue.isEmpty() && queue.peek() <= start) {
                 queue.poll();
@@ -25,11 +26,10 @@ class Solution {
         return queue.size();
     }
     
-    public int changeMin(String time) {
-        String t[] = time.split(":");
-        int hour = Integer.parseInt(t[0]) * 60;
-        int min = Integer.parseInt(t[1]);
+    public int changeToMinute (String time) {
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(3, 5));
         
-        return hour + min;
+        return hour * 60 + minute;
     }
 }
