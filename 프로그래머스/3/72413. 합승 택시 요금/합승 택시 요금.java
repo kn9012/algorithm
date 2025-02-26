52b7cb601c1f8a84c44d7553c71404036025a00a
Yuna <kn9012@naver.com>
2025-02-26 12:37:53 +0900

    [level 3] Title: 합승 택시 요금, Time: 26.10 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,60 +1,64 @@
 /**
  * 프로그래머스 합승 택시 요금
- * - 다익스트라
+ * - 다익스트라!
  */
 
 import java.util.*;
 
 class Solution {
-    class Node implements Comparable<Node> {
+    List<Node> list[];
+    int len;
+    
+    public class Node implements Comparable<Node> {
         int node, value;
         
-        Node (int node, int value) {
+        Node(int node, int value) {
             this.node = node;
             this.value = value;
         }
         
         @Override
-        public int compareTo(Node other) {
-            return Integer.compare(this.value, other.value);
+        public int compareTo(Node o) {
+            return Integer.compare(this.value, o.value);
         }
     }
     
-    List<Node> list[];
-    int[] dist;
-    
     public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = Integer.MAX_VALUE;
+        len = n;
+        
         list = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
         
         for (int i = 0; i < fares.length; i++) {
-            list[fares[i][0]].add(new Node(fares[i][1], fares[i][2]));
-            list[fares[i][1]].add(new Node(fares[i][0], fares[i][2]));
+            int start = fares[i][0];
+            int end = fares[i][1];
+            int value = fares[i][2];
+            
+            list[start].add(new Node(end, value));
+            list[end].add(new Node(start, value));
         }
         
-        int[] together = dijkstra(s, n);
-        int[] aloneA = dijkstra(a, n);
-        int[] aloneB = dijkstra(b, n);
-        
-        int charge = Integer.MAX_VALUE;
+        int[] together = dijstra(s);
+        int[] aloneA = dijstra(a);
+        int[] aloneB = dijstra(b);
         
         for (int i = 1; i <= n; i++) {
             if (together[i] == Integer.MAX_VALUE || aloneA[i] == Integer.MAX_VALUE || aloneB[i] == Integer.MAX_VALUE) continue;
-                
-            charge = Math.min(charge, together[i] + aloneA[i] + aloneB[i]);
+            answer = Math.min(answer, together[i] + aloneA[i] + aloneB[i]);
         }
         
-        return charge;
+        return answer;
     }
     
-    public int[] dijkstra(int s, int n) {
-        dist = new int[n + 1];
+    public int[] dijstra(int s) {
+        int[] dist = new int[len + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[s] = 0;
         
-        PriorityQueue<Node> queue = new PriorityQueue();
+        PriorityQueue<Node> queue = new PriorityQueue<>();
         queue.add(new Node(s, 0));
         
         while (!queue.isEmpty()) {
@@ -65,12 +69,11 @@ class Solution {
             if (dist[node] < value) continue;
             
             for (int i = 0; i < list[node].size(); i++) {
-                int nextN = list[node].get(i).node;
-                int nextV = list[node].get(i).value;
+                Node next = list[node].get(i);
                 
-                if (dist[nextN] > value + nextV) {
-                    queue.add(new Node(nextN, value + nextV));
-                    dist[nextN] = value + nextV;
+                if (dist[next.node] > value + next.value) {
+                    queue.add(new Node(next.node, value + next.value));
+                    dist[next.node] = value + next.value;
                 }
             }
         }
