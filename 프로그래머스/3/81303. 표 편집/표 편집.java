0717b66ab6ca5edd3bc1eb6fee6a0b6d1995a1e1
Yuna <kn9012@naver.com>
2025-03-06 13:53:47 +0900

    [level 3] Title: 표 편집, Time: 143.76 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -5,6 +5,8 @@ class Solution {
         int[] prev = new int[n];
         int[] next = new int[n];
         
+        Stack<Integer> removed = new Stack<>();
+        
         for (int i = 0; i < n; i++) {
             prev[i] = i - 1;
             next[i] = i + 1;
@@ -12,27 +14,35 @@ class Solution {
         
         next[n - 1] = -1;
         
-        Stack<Integer> removed = new Stack<>();
-        
         for (String c : cmd) {
-            char command = c.charAt(0);
+            String command = String.valueOf(c.charAt(0));
             
-            if (command == 'U') {
-                int move = Integer.parseInt(c.substring(2));
-                while (move-- > 0) k = prev[k];
-            } else if (command == 'D') {
-                int move = Integer.parseInt(c.substring(2));
-                while (move-- > 0) k = next[k];
-            } else if (command == 'C') {
-                removed.push(k);
-                if (prev[k] != -1) next[prev[k]] = next[k];
-                if (next[k] != -1) prev[next[k]] = prev[k];
-
-                k = (next[k] != -1) ? next[k] : prev[k];
-            } else if (command == 'Z') {
-                int restore = removed.pop();
-                if (prev[restore] != -1) next[prev[restore]] = restore;
-                if (next[restore] != -1) prev[next[restore]] = restore;
+            switch (command) {
+                case "U":
+                    int move = Integer.parseInt(c.substring(2));
+                    while (move-- > 0) k = prev[k];
+                    break;
+                
+                case "D":
+                    move = Integer.parseInt(c.substring(2));
+                    while (move-- > 0) k = next[k];
+                    break;
+                    
+                case "C":
+                    int size = removed.size();
+                    removed.push(k);
+                    
+                    if (prev[k] != -1) next[prev[k]] = next[k];
+                    if (next[k] != -1) prev[next[k]] = prev[k];
+                    
+                    k = (next[k] != -1) ? next[k] : prev[k];
+                    break;
+                    
+                case "Z":
+                    int num = removed.pop();
+                    if (prev[num] != -1) next[prev[num]] = num;
+                    if (next[num] != -1) prev[next[num]] = num;
+                    break;
             }
         }
         
@@ -40,7 +50,7 @@ class Solution {
         while (!removed.isEmpty()) {
             sb.setCharAt(removed.pop(), 'X');
         }
-
+        
         return sb.toString();
     }
 }
