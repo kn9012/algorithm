de1c6dd31468a49b3c136df6d7f65f687773029b
Yuna <kn9012@naver.com>
2025-03-06 16:05:01 +0900

    [level 3] Title: 길 찾기 게임, Time: 43.49 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -3,41 +3,38 @@ import java.util.*;
 class Solution {
     class Node {
         int x, y, v;
-        
         Node left, right;
         
-        Node (int x, int y, int v) {
+        Node(int x, int y, int v) {
             this.x = x;
             this.y = y;
             this.v = v;
         }
     }
     
-    List<Node> nodes = new ArrayList<>();
     int[][] answer;
     int count = 0;
+    List<Node> list;
     
     public int[][] solution(int[][] nodeinfo) {
-        int size = nodeinfo.length;
+        answer = new int[2][nodeinfo.length];
+        list = new ArrayList<>();
         
-        for (int i = 0; i < size; i++) {
-            nodes.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
+        for (int i = 0; i < nodeinfo.length; i++) {
+            list.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
         }
         
-        Collections.sort(nodes, (o1, o2) -> {
-            return Integer.compare(o2.y, o1.y);
-        });
+        Collections.sort(list, (o1, o2) -> Integer.compare(o2.y, o1.y));
         
-        Node root = nodes.get(0);
+        Node root = list.get(0);
         
-        for (int i = 1; i < size; i++) {
-            find(root, nodes.get(i));
+        for (int i = 1; i < list.size(); i++) {
+            find(root, list.get(i));
         }
         
-        answer = new int[2][size];
-        pre(root);
+        preorder(root);
         count = 0;
-        post(root);
+        postorder(root);
         
         return answer;
     }
@@ -52,18 +49,18 @@ class Solution {
         }
     }
     
-    public void pre(Node root) {
+    public void preorder(Node root) {
         if (root != null) {
             answer[0][count++] = root.v;
-            pre(root.left);
-            pre(root.right);
+            preorder(root.left);
+            preorder(root.right);
         }
     }
     
-    public void post(Node root) {
+    public void postorder(Node root) {
         if (root != null) {
-            post(root.left);
-            post(root.right);
+            postorder(root.left);
+            postorder(root.right);
             answer[1][count++] = root.v;
         }
     }
