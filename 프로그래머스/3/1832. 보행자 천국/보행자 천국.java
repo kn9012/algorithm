0637bbf3b3fd74084e60c942ffa39bfa34d52b9b
Yuna <kn9012@naver.com>
2025-03-07 21:24:14 +0900

    [level 3] Title: 보행자 천국, Time: 115.88 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,25 +1,23 @@
 class Solution {
-	  int MOD = 20170805;
-	  public int solution(int m, int n, int[][] cityMap) {
-	      // dp[][][0] 아래쪽, dp[][][1] 오른쪽
-          int[][][] dp = new int[m + 1][n + 1][2];
-	      
-          dp[1][1][0] = dp[1][1][1] = 1;
-          
-          for(int r = 1 ; r <= m ; ++r){
-              for(int c = 1 ; c <= n ; ++c){
-                  if(cityMap[r - 1][c - 1] == 0){
-                      dp[r][c][0] += (dp[r - 1][c][0] + dp[r][c - 1][1]) % MOD;
-                      dp[r][c][1] += (dp[r - 1][c][0] + dp[r][c - 1][1]) % MOD;
-                  } else if(cityMap[r - 1][c - 1] == 1){
-                      dp[r][c][0] = 0;
-                      dp[r][c][1] = 0;
-                  } else {
-                      dp[r][c][0] = dp[r - 1][c][0];
-                      dp[r][c][1] = dp[r][c - 1][1];
-                  }
-              }
-          }
-          return dp[m][n][0]; 
-      }
+    int MOD = 20170805;
+    
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][][] dp = new int[m + 1][n + 1][2];
+        
+        dp[0][0][0] = 1;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cityMap[i][j] == 0) { // 자동차가 자유롭게 지나갈 수 있음
+                    dp[i + 1][j][0] += (dp[i][j][0] + dp[i][j][1]) % MOD;
+                    dp[i][j + 1][1] += (dp[i][j][0] + dp[i][j][1]) % MOD;
+                } else if (cityMap[i][j] == 2) { // 좌회전이나 우회전 금지
+                    dp[i + 1][j][0] += dp[i][j][0] % MOD;
+                    dp[i][j + 1][1] += dp[i][j][1] % MOD;
+                }
+            }
+        }
+        
+        return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD;
+    }
 }
