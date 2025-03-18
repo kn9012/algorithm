85d6dd831573eb6b128bef1db12a5878446b149c
Yuna <kn9012@naver.com>
2025-03-18 17:06:31 +0900

    [Gold IV] Title: 좋다, Time: 124 ms, Memory: 12736 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -6,43 +6,55 @@ import java.util.StringTokenizer;
 
 /**
  * 백준 1253번 좋다
- * - 투 포인터
+ * 어떤 수가 다른 두 개의 수의 합으로 나타낼 수 있으면 좋은 수
+ * 수의 위치가 다르면 값이 같아도 다른 수 
+ * => 이분탐색 사용
  */
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int nums[] = new int[N];
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int count = 0;
-		
 		Arrays.sort(nums);
+		
+		int answer = 0;
 		
 		for (int i = 0; i < N; i++) {
 			int left = 0;
 			int right = N - 1;
 			
-			while (true) {
-				if (i == left) left++;
-				if (i == right) right--;
+			while (left < right) {
+				int sum = nums[left] + nums[right];
 				
-				if (left >= right) break;
+				if (i == left) {
+					left++;
+					continue;
+				}
+				if (i == right) {
+					right--;
+					continue;
+				}
 				
-				if (nums[left] + nums[right] == nums[i]) {
-					count++;
+				if (sum < nums[i]) {
+					left++;
+				} else if (sum > nums[i]) {
+					right--;
+				} else {
+					answer++;
 					break;
 				}
-				else if (nums[left] + nums[right] > nums[i]) right--;
-				else left++;
-			}
+			} 
 		}
 		
-		System.out.println(count);
+		System.out.println(answer);
 	}
 }
