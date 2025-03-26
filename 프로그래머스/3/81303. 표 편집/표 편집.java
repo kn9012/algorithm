fb04756163910898e6133cf118a16b859cc32482
Yuna <kn9012@naver.com>
2025-03-26 14:19:04 +0900

    [level 3] Title: 표 편집, Time: 134.94 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -5,7 +5,7 @@ class Solution {
         int[] prev = new int[n];
         int[] next = new int[n];
         
-        Stack<Integer> removed = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < n; i++) {
             prev[i] = i - 1;
@@ -15,40 +15,33 @@ class Solution {
         next[n - 1] = -1;
         
         for (String c : cmd) {
-            String command = String.valueOf(c.charAt(0));
+            String command = c.substring(0, 1);
             
-            switch (command) {
-                case "U":
-                    int move = Integer.parseInt(c.substring(2));
-                    while (move-- > 0) k = prev[k];
-                    break;
+            if (command.equals("U")) {
+                int X = Integer.parseInt(c.substring(2));
+                while (X-- > 0) k = prev[k];
+            } else if (command.equals("D")) {
+                int X = Integer.parseInt(c.substring(2));
+                while (X-- > 0) k = next[k];
+            } else if (command.equals("C")) {
+                stack.push(k);
                 
-                case "D":
-                    move = Integer.parseInt(c.substring(2));
-                    while (move-- > 0) k = next[k];
-                    break;
-                    
-                case "C":
-                    int size = removed.size();
-                    removed.push(k);
-                    
-                    if (prev[k] != -1) next[prev[k]] = next[k];
-                    if (next[k] != -1) prev[next[k]] = prev[k];
-                    
-                    k = (next[k] != -1) ? next[k] : prev[k];
-                    break;
-                    
-                case "Z":
-                    int num = removed.pop();
-                    if (prev[num] != -1) next[prev[num]] = num;
-                    if (next[num] != -1) prev[next[num]] = num;
-                    break;
+                if (next[k] != -1) prev[next[k]] = prev[k];
+                if (prev[k] != -1) next[prev[k]] = next[k];
+                
+                k = (next[k] != -1) ? next[k] : prev[k];
+            } else {
+                int K = stack.pop();
+                
+                if (prev[K] != -1) next[prev[K]] = K;
+                if (next[K] != -1) prev[next[K]] = K;
             }
         }
         
         StringBuilder sb = new StringBuilder("O".repeat(n));
-        while (!removed.isEmpty()) {
-            sb.setCharAt(removed.pop(), 'X');
+        
+        while (!stack.isEmpty()) {
+            sb.setCharAt(stack.pop(), 'X');
         }
         
         return sb.toString();
