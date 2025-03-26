01174ed34752d8318b5b39e26e1a7fdec4a0baa5
Yuna <kn9012@naver.com>
2025-03-26 15:33:44 +0900

    [level 3] Title: 길 찾기 게임, Time: 32.58 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,67 +1,71 @@
 import java.util.*;
 
 class Solution {
-    class Node {
-        int x, y, v;
+    public class Node implements Comparable<Node> {
+        int x, y, idx;
         Node left, right;
         
-        Node(int x, int y, int v) {
+        Node (int x, int y, int idx) {
             this.x = x;
             this.y = y;
-            this.v = v;
+            this.idx = idx;
+        }
+        
+        // y 좌표값이 가장 큰 좌표가 root 노드이므로 y 좌표값 기준 내림차순 정렬
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(o.y, this.y);
         }
     }
     
-    int[][] answer;
-    int count = 0;
-    List<Node> list;
+    public int[][] answer;
+    public int index = 0;
     
     public int[][] solution(int[][] nodeinfo) {
         answer = new int[2][nodeinfo.length];
-        list = new ArrayList<>();
         
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         for (int i = 0; i < nodeinfo.length; i++) {
-            list.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
+            pq.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
         }
         
-        Collections.sort(list, (o1, o2) -> Integer.compare(o2.y, o1.y));
+        Node root = pq.poll();
         
-        Node root = list.get(0);
-        
-        for (int i = 1; i < list.size(); i++) {
-            find(root, list.get(i));
-        }
+        while (!pq.isEmpty()) findChild(root, pq.poll());
         
         preorder(root);
-        count = 0;
+        index = 0;
         postorder(root);
         
         return answer;
     }
     
-    public void find(Node parent, Node child) {
-        if (parent.x > child.x) {
-            if (parent.left == null) parent.left = child;
-            else find(parent.left, child);
-        } else {
-            if (parent.right == null) parent.right = child;
-            else find(parent.right, child);
+    public void findChild(Node root, Node child) {
+        // x 좌표가 root 노드보다 작으면 왼쪽 자식
+        if (child.x < root.x) {
+            if (root.left == null) root.left = child;
+            else findChild(root.left, child);
+        } else { // x 좌표가 root 노드보다 크다면 오른쪽 자식
+            if (root.right == null) root.right = child;
+            else findChild(root.right, child);
         }
     }
     
-    public void preorder(Node root) {
-        if (root != null) {
-            answer[0][count++] = root.v;
-            preorder(root.left);
-            preorder(root.right);
+    // 전위순회 : root -> 왼 -> 오
+    public void preorder(Node node) {
+        if (node != null) {
+            answer[0][index++] = node.idx;
+            preorder(node.left);
+            preorder(node.right);
         }
     }
     
-    public void postorder(Node root) {
-        if (root != null) {
-            postorder(root.left);
-            postorder(root.right);
-            answer[1][count++] = root.v;
+    // 후위순회 : 왼 -> 오 -> root
+    public void postorder(Node node) {
+        if (node != null) {
+            postorder(node.left);
+            postorder(node.right);
+            answer[1][index++] = node.idx;
         }
     }
 }
