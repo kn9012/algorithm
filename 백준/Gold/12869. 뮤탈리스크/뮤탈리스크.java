973bec73fce1ccc889c29e6aa2116e91d12278e3
Yuna <kn9012@naver.com>
2025-03-27 19:10:22 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 80 ms, Memory: 12988 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int[] scv;
 	public static int min = Integer.MAX_VALUE;
 	public static boolean[][][] visited;
 	
@@ -15,7 +14,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		scv = new int[3];
+		int[] scv = new int[3];
 		visited = new boolean[61][61][61];
 		
 		for (int i = 0; i < N; i++) {
@@ -37,24 +36,20 @@ public class Main {
 			return;
 		}
 		
-		Integer[] sort = {s1, s2, s3};
-		Arrays.sort(sort, Collections.reverseOrder());
+		Integer[] s = {s1, s2, s3};
+		Arrays.sort(s, Collections.reverseOrder());
 		
-		s1 = sort[0];
-		s2 = sort[1];
-		s3 = sort[2];
-		
-		if (visited[s1][s2][s3]) return;
-		else visited[s1][s2][s3] = true;
+		if (visited[s[0]][s[1]][s[2]]) return;
+		else visited[s[0]][s[1]][s[2]] = true;
 		
 		if (count > min) return;
 		
-		dfs(s1 - 9, s2 - 3, s3 - 1, count + 1);
-		dfs(s1 - 9, s2 - 1, s3 - 3, count + 1);
-		dfs(s1 - 3, s2 - 9, s3 - 1, count + 1);
-		dfs(s1 - 3, s2 - 1, s3 - 9, count + 1);
-		dfs(s1 - 1, s2 - 9, s3 - 3, count + 1);
-		dfs(s1 - 1, s2 - 3, s3 - 9, count + 1);
+		dfs(s[0] - 9, s[1] - 3, s[2] - 1, count + 1);
+		dfs(s[0] - 9, s[1] - 1, s[2] - 3, count + 1);
+		dfs(s[0] - 3, s[1] - 9, s[2] - 1, count + 1);
+		dfs(s[0] - 3, s[1] - 1, s[2] - 9, count + 1);
+		dfs(s[0] - 1, s[1] - 9, s[2] - 3, count + 1);
+		dfs(s[0] - 1, s[1] - 3, s[2] - 9, count + 1);
 		
 		return;
 	}
