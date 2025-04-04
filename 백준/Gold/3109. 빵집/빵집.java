eec8a5931e7b1e99f58cde21da4a7cb1cc6100bc
Yuna <kn9012@naver.com>
2025-04-04 13:57:18 +0900

    [Gold II] Title: 빵집, Time: 320 ms, Memory: 41892 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -3,41 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * [문제] 백준 3109번 빵집
- * [아이디어] 첫째 열부터 마지막 열로만 이동이 가능하며 건물에는 파이프를 놓을 수 없음
- * 오른쪽, 오른쪽 위, 오른쪽 아래만 가능
- * 
- * 실행 시간 : ms
- * 메모리 : kb
- * 
- * @author 김유나
- * 2023-11-16
- *
- */
-
 public class Main {
-	static String map[][];
-	static int R, C, result, deltas[][] = {{-1, 1}, {0, 1}, {1, 1}};
-	static boolean visited[][], flag;
+	public static int R, C, result = 0;
+	public static int[][] deltas = {{-1, 1}, {0, 1}, {1, 1}};
+	public static char[][] map;
+	public static boolean[][] visited;
+	public static boolean flag;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		map = new String[R][C];
-		visited = new boolean[R][C];
-		result = 0;
-		
+		map = new char[R][C];
 		for (int i = 0; i < R; i++) {
-			String str = br.readLine();
+			String s = br.readLine();
 			for (int j = 0; j < C; j++) {
-				map[i][j] = str.charAt(j) + "";
+				map[i][j] = s.charAt(j); 
 			}
 		}
 		
+		visited = new boolean[R][C];
 		for (int i = 0; i < R; i++) {
 			flag = false;
 			dfs(i, 0);
@@ -45,28 +33,24 @@ public class Main {
 		
 		System.out.println(result);
 	}
-	static void dfs(int x, int y) {
+	
+	public static void dfs(int x, int y) {
 		if (y == C - 1) {
 			result++;
 			flag = true;
-			visited[x][y] = true;
 			return;
 		}
 		
-	
 		for (int i = 0; i < 3; i++) {
 			int dx = x + deltas[i][0];
 			int dy = y + deltas[i][1];
 			
-			if (dx < 0 || dy < 0 || dx >= R || dy >= C || visited[dx][dy] || map[dx][dy].equals("x")) continue;
+			if (dx < 0 || dy < 0 || dx >= R || dy >= C || visited[dx][dy] || map[dx][dy] == 'x') continue;
+			
+			if (flag) return;
+			else visited[dx][dy] = true;
 			
 			dfs(dx, dy);
-				
-			if (flag) {
-				visited[x][y] = true;
-				return;
-			}
-			else visited[x][y] = true;
 		}
 	}
 }
