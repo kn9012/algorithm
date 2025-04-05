2ca02af03e2af33d596ca5a6fafe83f9f7c1149d
Yuna <kn9012@naver.com>
2025-04-05 17:48:23 +0900

    [Gold II] Title: 공항, Time: 184 ms, Memory: 22184 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -4,46 +4,46 @@ import java.io.InputStreamReader;
 
 /**
  * 백준 10775번 공항
- *
+ * - Union-Find 
  */
 
 public class Main {
-	public static int[] parent;
-	
+	public static int[] parent, gate;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int G = Integer.parseInt(br.readLine());
 		int P = Integer.parseInt(br.readLine());
 		
 		parent = new int[G + 1];
-		for (int i = 0; i <= G; i++) {
-			parent[i] = i;
-		}
-		
 		int count = 0;
+		
+		for (int i = 1; i <= G; i++) parent[i] = i;
+		
 		for (int i = 0; i < P; i++) {
 			int g = Integer.parseInt(br.readLine());
 			
 			if (find(g) == 0) break;
 			
 			count++;
+			
 			union(find(g) - 1, find(g));
 		}
 		
 		System.out.println(count);
 	}
 	
-	public static int find(int a) {
-		if (parent[a] == a) return a;
-		return parent[a] = find(parent[a]);
+	public static int find(int n) {
+		if (parent[n] == n) return n;
+		return parent[n] = find(parent[n]);
 	}
 	
-	public static void union(int prev, int next) {
-		int prevRoot = find(prev);
-		int nextRoot = find(next);
+	public static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
 		
-		if (prevRoot != nextRoot) {
-			parent[nextRoot] = find(prevRoot);
+		if (a != b) {
+			parent[b] = a;
 		}
 	}
 }
