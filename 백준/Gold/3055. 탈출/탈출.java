483515c9b9539c33ba2d9a25e6431d9849323760
Yuna <kn9012@naver.com>
2025-05-02 15:50:05 +0900

    [Gold IV] Title: 탈출, Time: 72 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -6,85 +6,99 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * [문제] 백준 3055번 탈출 [아이디어] BFS
- * 
- * 실행 시간 : ms 메모리 : kb
- * 
- * @author 김유나 2023-11-14
- *
+ * 백준 3055번 탈출
+ * - 
  */
-public class Main {
-	static String map[][];
-	static int R, C, min, deltas[][] = { { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 0 } };
-	static Queue<int[]> queue;
-	static Queue<int[]> water;
 
+public class Main {
+	public static int[][] deltas = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-
-		map = new String[R][C];
-		queue = new ArrayDeque<>();
-		water = new ArrayDeque<>();
-
+		
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		char[][] map = new char[R][C];
+		
+		Queue<int []> water = new ArrayDeque<>();
+		Queue<int []> hedgehog = new ArrayDeque<>();
+		
+		boolean[][] wVisited = new boolean[R][C];
+		boolean[][] hVisited = new boolean[R][C];
+		
+		int[] beaver = new int[2];
+		
 		for (int i = 0; i < R; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < C; j++) {
-				map[i][j] = str.charAt(j) + "";
-
-				if (map[i][j].equals("S"))
-					queue.add(new int[] { i, j, 0 });
-				else if (map[i][j].equals("*"))
-					water.add(new int[] { i, j, 0 });
-			}
-		}
-
-		min = Integer.MAX_VALUE;
-		bfs();
-
-		System.out.println(min == Integer.MAX_VALUE ? "KAKTUS" : min);
-	}
-
-	static void bfs() {
-		while (!queue.isEmpty()) {
-			int len = water.size();
-			for (int i = 0; i < len; i++) {
-				int waters[] = water.poll();
-				for (int j = 0; j < 4; j++) {
-					int wx = waters[0] + deltas[j][0];
-					int wy = waters[1] + deltas[j][1];
-
-					if (wx >= 0 && wy >= 0 && wx < R && wy < C) {
-						if (map[wx][wy].equals(".")) {
-							map[wx][wy] = "*";
-							water.add(new int[] { wx, wy, 0 });
-						}
-					}
+				map[i][j] = str.charAt(j);
+				
+				if (map[i][j] == 'S') {
+					hedgehog.offer(new int[] {i, j, 0});
+					hVisited[i][j] = true;
+				} else if (map[i][j] == '*') {
+					water.offer(new int[] {i, j});
+					wVisited[i][j] = true;
+				} else if (map[i][j] == 'D') {
+					beaver[0] = i;
+					beaver[1] = j;
 				}
 			}
-
-			len = queue.size();
-			for (int i = 0; i < len; i++) {
-				int cur[] = queue.poll();
+		}
+		
+		int answer = -1;
+		
+		while (!hedgehog.isEmpty()) {
+			int[] hCur = hedgehog.peek();
+			
+			if (hCur[0] == beaver[0] && hCur[1] == beaver[1]) {
+				
+			}
+			
+			int size = water.size();
+			
+			for (int i = 0; i < size; i++) {
+				int[] wCur = water.poll();
+				
 				for (int j = 0; j < 4; j++) {
-					int dx = cur[0] + deltas[j][0];
-					int dy = cur[1] + deltas[j][1];
-
-					if (dx >= 0 && dy >= 0 && dx < R && dy < C) {
-						if (map[dx][dy].equals("D")) {
-							min = Math.min(min, cur[2] + 1);
-							return;
-						}
-						else if (map[dx][dy].equals(".")) {
-							map[dx][dy] = "S";
-							queue.add(new int[] { dx, dy, cur[2] + 1 });
-						}
+					int wx = wCur[0] + deltas[j][0];
+					int wy = wCur[1] + deltas[j][1];
+					
+					if (wx >= R || wy >= C || wx < 0 || wy < 0 || map[wx][wy] == 'D' || map[wx][wy] == 'X') continue;
+					
+					if (map[wx][wy] == '.') {
+						water.offer(new int[] {wx, wy});
+						map[wx][wy] = '*';
 					}
 				}
 			}
 			
+			size = hedgehog.size();
+			
+			for (int i = 0; i < size; i++) {
+				hCur = hedgehog.poll();
+				
+				for (int j = 0; j < 4; j++) {
+					int dx = hCur[0] + deltas[j][0];
+					int dy = hCur[1] + deltas[j][1];
+					
+					if (dx >= R || dy >= C || dx < 0 || dy < 0 || map[dx][dy] == '*' || map[dx][dy] == 'X') continue;
+					
+					if (map[dx][dy] == 'D') {
+						answer = hCur[2] + 1;
+						System.out.println(answer);
+						return;
+					} else if (map[dx][dy] == '.') {
+						map[dx][dy] = 'S';
+						hedgehog.offer(new int[] {dx, dy, hCur[2] + 1});
+					}
+					
+				}
+			}
 		}
+		
+		System.out.println("KAKTUS");
 	}
 }
