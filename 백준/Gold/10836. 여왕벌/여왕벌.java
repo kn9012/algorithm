ddac8960cd31d917a4aea7c9cd1802fa8a0b36ea
Yuna <kn9012@naver.com>
2025-06-01 18:29:02 +0900

    [Gold III] Title: 여왕벌, Time: 4020 ms, Memory: 318696 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * 백준 10836번 여왕벌
- * 
+ * - 시뮬레이션 + 최적화
  */
 
 public class Main {
@@ -17,58 +16,40 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken()); // 한 변의 크기
 		int N = Integer.parseInt(st.nextToken()); // 날짜
 		
-		int[][] honeycomb = new int[M][M];
-		for (int i = 0; i < M; i++) Arrays.fill(honeycomb[i], 1);
+		int[] growth = new int[2 * M - 1];
 				
-		for (int i = 0; i < N; i++) {
+		for (int day = 0; day < N; day++) {
 			st = new StringTokenizer(br.readLine());
 			int zero = Integer.parseInt(st.nextToken());
 			int one = Integer.parseInt(st.nextToken());
 			int two = Integer.parseInt(st.nextToken());
 			
-			int x = M - 1;
-			int y = 0;
-			
-			for (int j = 0; j < zero; j++) {
-				if (x > 0) x--;
-				else y++;
-			}
-			
-			for (int j = 0; j < one; j++) {
-				if (x > 0) {
-					honeycomb[x][y] += 1;
-					x--;
-				} else {
-					honeycomb[x][y] += 1;
-					
-					if (y > 0) {
-						for (int k = 1; k < M; k++) honeycomb[x + k][y] += 1;
-					}
-					
-					y++;
-				}
-			}
-			
-			for (int j = 0; j < two; j++) {
-				if (x > 0) {
-					honeycomb[x][y] += 2;
-					x--;
-				} else {
-					honeycomb[x][y] += 2;
-					
-					if (y > 0) {
-						for (int k = 1; k < M; k++) honeycomb[x + k][y] += 2;
-					}
-					
-					y++;
-				}
-			}
+			int idx = 0;
+            while (zero-- > 0) idx++;
+            while (one-- > 0) growth[idx++] += 1;
+            while (two-- > 0) growth[idx++] += 2;
 		}
 		
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < M; j++) {
-				System.out.print(honeycomb[i][j] + " ");
-			} System.out.println();
-		}
+		int[][] honeycomb = new int[M][M];
+		
+        for (int i = 0; i < M; i++) {
+            honeycomb[M - 1 - i][0] = 1 + growth[i];
+        }
+
+        for (int j = 1; j < M; j++) {
+            honeycomb[0][j] = 1 + growth[M - 1 + j];
+        }
+
+        for (int i = 1; i < M; i++) {
+            for (int j = 1; j < M; j++) {
+                honeycomb[i][j] = honeycomb[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(honeycomb[i][j] + " ");
+            } System.out.println();
+        }
 	}
 }
