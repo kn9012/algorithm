1d8e40308bc8e3c6b59cb24a2c769b0cf9d9f7c0
Yuna <kn9012@naver.com>
2025-06-06 13:13:21 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 265.87 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -1,47 +1,42 @@
 import java.util.*;
 
+/**
+ * 프로그래머스 무지의 먹방 라이브
+ * - 섭취 시간 오름차순으로 정렬 후 묶어서 계산
+ */
+
 class Solution {
-    class Food {
-        int index, time;
-        
-        Food (int index, int time) {
-            this.index = index;
-            this.time = time;
-        }
-    }
-    
     public int solution(int[] food_times, long k) {
-        PriorityQueue<Food> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.time, o2.time));
+        // 섭취 시간 오름차순으로 정렬
+        PriorityQueue<int []> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
         for (int i = 0; i < food_times.length; i++) {
-            pq.add(new Food(i, food_times[i]));
+            pq.add(new int[] {food_times[i], i});       
         }
         
-        long prevTime = 0;
+        long prevFoodTime = 0;
         
         while (!pq.isEmpty()) {
+            int[] curFood = pq.peek();
             int size = pq.size();
             
-            Food first = pq.peek();
-            
-            if (k - (size * (long) (first.time - prevTime)) >= 0) {
-                k -= size * (long) (first.time - prevTime);
-                
-                prevTime = first.time;
+            // 한 사이클이 k보다 작다면
+            // 해당 사이클 빼주고 직전 음식 시간 갱신
+            if (size * (curFood[0] - prevFoodTime) <= k) {
+                k -= size * (curFood[0] - prevFoodTime);
+                prevFoodTime = curFood[0];
                 pq.poll();
-                //System.out.println(k);
-            } else break;
+            } else {
+                break;
+            }
         }
-        
-        List<Food> list = new ArrayList<>();
         
         if (pq.isEmpty()) return -1;
         
-        while (!pq.isEmpty()) {
-            list.add(pq.poll());
-        }
+        List<int []> list = new ArrayList<>();
+        while (!pq.isEmpty()) list.add(pq.poll());
         
-        Collections.sort(list, (o1, o2) -> Integer.compare(o1.index, o2.index));
+        Collections.sort(list, (o1, o2) -> Integer.compare(o1[1], o2[1]));
         
-        return list.get((int) (k % list.size())).index + 1;
+        return list.get((int) (k % list.size()))[1] + 1;
     }
 }
