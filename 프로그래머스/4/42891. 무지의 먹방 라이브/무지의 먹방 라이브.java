07d4af94aea1bfa3920770aa9dec1eec53172d52
Yuna <kn9012@naver.com>
2025-06-13 10:44:38 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 262.74 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -1,42 +1,37 @@
 import java.util.*;
 
-/**
- * 프로그래머스 무지의 먹방 라이브
- * - 섭취 시간 오름차순으로 정렬 후 묶어서 계산
- */
-
 class Solution {
     public int solution(int[] food_times, long k) {
-        // 섭취 시간 오름차순으로 정렬
-        PriorityQueue<int []> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
-        for (int i = 0; i < food_times.length; i++) {
-            pq.add(new int[] {food_times[i], i});       
+        PriorityQueue<int []> foods = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
+        
+        int index = 0;
+        for (int times : food_times) {
+            foods.offer(new int[] {times, index++});
         }
         
         long prevFoodTime = 0;
         
-        while (!pq.isEmpty()) {
-            int[] curFood = pq.peek();
-            int size = pq.size();
+        while (!foods.isEmpty()) {
+            int time = foods.peek()[0];
             
-            // 한 사이클이 k보다 작다면
-            // 해당 사이클 빼주고 직전 음식 시간 갱신
-            if (size * (curFood[0] - prevFoodTime) <= k) {
-                k -= size * (curFood[0] - prevFoodTime);
-                prevFoodTime = curFood[0];
-                pq.poll();
+            if ((time - prevFoodTime) * foods.size() <= k) {
+                k -= (time - prevFoodTime) * foods.size();
+                prevFoodTime = time;
+                foods.poll();
             } else {
                 break;
             }
         }
         
-        if (pq.isEmpty()) return -1;
+        if (foods.isEmpty()) return -1;
         
-        List<int []> list = new ArrayList<>();
-        while (!pq.isEmpty()) list.add(pq.poll());
+        List<int []> remain = new ArrayList<>();
+        while (!foods.isEmpty()) {
+            remain.add(foods.poll());
+        }
         
-        Collections.sort(list, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+        Collections.sort(remain, (o1, o2) -> Integer.compare(o1[1], o2[1]));
         
-        return list.get((int) (k % list.size()))[1] + 1;
+        return remain.get((int) (k % remain.size()))[1] + 1;
     }
 }
