3510a9eb8f3ce1ae9261e9423ba5cb8c2548d960
Yuna <kn9012@naver.com>
2025-08-16 14:53:59 +0900

    [level 3] Title: 베스트앨범, Time: 3.79 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,44 +1,51 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-    public List<Integer> solution(String[] genres, int[] plays) {
+    class Music implements Comparable<Music> {
+        int index;
+        int play;
+        String genre;
+        
+        Music (int index, int play, String genre) {
+            this.index = index;
+            this.play = play;
+            this.genre = genre;
+        }
+        
+        @Override
+        public int compareTo(Music o) {
+            if (this.play == o.play) {
+                return Integer.compare(this.index, o.index);
+            } else return Integer.compare(o.play, this.play);
+        }
+    }
+    
+    public int[] solution(String[] genres, int[] plays) {
+        List<Music> musicList = new ArrayList<>();
         Map<String, Long> genrePlay = new HashMap<>();
-        Map<String, ArrayList<int []>> sing = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < genres.length; i++) {            
+        for (int i = 0; i < genres.length; i++) {
             genrePlay.put(genres[i], genrePlay.getOrDefault(genres[i], 0L) + plays[i]);
-            
-            if (sing.containsKey(genres[i])) {
-                sing.get(genres[i]).add(new int[] {i, plays[i]});
-            } else {
-                ArrayList<int []> temp = new ArrayList<>();
-                temp.add(new int[] {i, plays[i]});
-                sing.put(genres[i], temp);
-            }
+            musicList.add(new Music(i, plays[i], genres[i]));
         }
         
-        List<Map.Entry<String, Long>> collect = genrePlay.entrySet().stream().sorted((o1, o2) -> {
-                    return -(o1.getValue().compareTo(o2.getValue()));
-                }).collect(Collectors.toList());
+        List<String> sortedGenres = new ArrayList<>(genrePlay.keySet());
+        sortedGenres.sort((a, b) -> Long.compare(genrePlay.get(b), genrePlay.get(a)));
         
-        for (int i = 0; i < genrePlay.size(); i++) {
-            Map.Entry<String, Long> entry = collect.get(i);
-            ArrayList<int[]> temp = sing.get(entry.getKey());
-            Collections.sort(temp, (o1, o2) -> {
-                return -(o1[1] - o2[1]);
-            });
-            
-            for(int j = 0; j < temp.size(); j++){
-                if(j == 2) break;
-                
-                int [] check = temp.get(j);
-                list.add(check[0]);
+        List<Integer> answer = new ArrayList<>();
+        
+        // 각 장르에서 최대 2곡 선택
+        for (String genre : sortedGenres) {
+            List<Music> list = new ArrayList<>();
+            for (Music m : musicList) {
+                if (m.genre.equals(genre)) list.add(m);
             }
+            Collections.sort(list); // 재생 수 내림차순, 같으면 index 오름차순
+            
+            answer.add(list.get(0).index);
+            if (list.size() > 1) answer.add(list.get(1).index);
         }
         
-        
-        return list;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
