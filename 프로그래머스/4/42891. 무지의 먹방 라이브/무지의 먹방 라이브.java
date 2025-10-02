7ef5879e8d35aa18f9c5d89dc4534d5f0ad5c553
Yuna <kn9012@naver.com>
2025-10-02 15:18:04 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 273.90 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] food_times, long k) {
-        PriorityQueue<int []> foods = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
+    public long solution(int[] food_times, long k) {
+        // 걸리는 시간이 작은 순서대로 정렬
+        // [0] : 인덱스, [1] : 걸리는 시간
+        PriorityQueue<long []> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[1], o2[1]));
         
-        int index = 0;
-        for (int times : food_times) {
-            foods.offer(new int[] {times, index++});
+        int index = 1;
+        // [2, 1], [3, 2], [1, 3] 순으로 정렬됨
+        for (int time : food_times) {
+            pq.offer(new long[] {index++, time});
         }
         
-        long prevFoodTime = 0;
+        long prevValue = 0;
         
-        while (!foods.isEmpty()) {
-            int time = foods.peek()[0];
+        while (!pq.isEmpty()) {
+            // 효율성 테스트에서는 pq.size()가 200,000 이하, pq.peek()이 100,000,000 이하 -> long
+            long cycleSize = pq.size() * (pq.peek()[1] - prevValue);
             
-            if ((time - prevFoodTime) * foods.size() <= k) {
-                k -= (time - prevFoodTime) * foods.size();
-                prevFoodTime = time;
-                foods.poll();
-            } else {
-                break;
-            }
+            // 한 사이클의 크기보다 k가 크다면 한 사이클 빼기
+            if (k >= cycleSize) {
+                k -= cycleSize;
+                prevValue = pq.poll()[1];
+            } else break;
         }
         
-        if (foods.isEmpty()) return -1;
-        
-        List<int []> remain = new ArrayList<>();
-        while (!foods.isEmpty()) {
-            remain.add(foods.poll());
+        List<long []> list = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            list.add(pq.poll());
         }
         
-        Collections.sort(remain, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+        Collections.sort(list, (o1, o2) -> Long.compare(o1[0], o2[0]));
         
-        return remain.get((int) (k % remain.size()))[1] + 1;
+        return (list.size() == 0 ? -1 : list.get((int) (k % list.size()))[0]);
     }
 }
