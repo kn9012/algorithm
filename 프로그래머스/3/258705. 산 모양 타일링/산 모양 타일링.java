8c557c83e7567048157c81ccd8dfd844920c4a2d
Yuna <kn9012@naver.com>
2025-10-05 14:13:33 +0900

    [level 3] Title: 산 모양 타일링, Time: 15.84 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -2,20 +2,29 @@ class Solution {
     private static final int MOD = 10007;
 
     public int solution(int n, int[] tops) {
-        // n >= 1 이라고 가정
-        int[] dp1 = new int[n]; // 마름모로 끝나는 경우
-        int[] dp2 = new int[n]; // 마름모가 아닌 경우
+        // dp[i][0]: i번째 열이 마름모로 끝나는 경우의 수
+        // dp[i][1]: i번째 열이 세모로 끝나는 경우의 수
+        long[][] dp = new long[n + 1][2];
 
-        dp1[0] = 1;
-        dp2[0] = 2 + tops[0]; // tops[0]==0 -> 2, tops[0]==1 -> 3
+        // 기본 값 세팅
+        // 첫번째 열이 마름모로 끝날 경우 tops가 있어도 경우의 수는 1로 같음
+        dp[1][0] = 1;
+        // 세모로 끝날 경우 tops가 없으면 2가지, tops가 있으면 3가지
+        dp[1][1] = 2 + tops[0];
 
-        for (int i = 1; i < n; i++) {
-            dp1[i] = (dp1[i - 1] + dp2[i - 1]) % MOD;
-            long term1 = (long) dp1[i - 1] * (1 + tops[i]);
-            long term2 = (long) dp2[i - 1] * (2 + tops[i]);
-            dp2[i] = (int) ((term1 + term2) % MOD);
+        for (int i = 2; i <= n; i++) {
+            int top = tops[i - 1];
+
+            // i번째가 마름모로 끝나는 경우
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+
+            // i-1번째가 마름모였을 경우 세모 + tops가 있으면 4번 마름모
+            long fromRhombus = dp[i - 1][0] * (1 + top);
+            // i-1번째가 마름모가 아닐 경우 2번 마름모, 세모 + tops가 있으면 4번 마름모
+            long fromNotRhombus = dp[i - 1][1] * (2 + top);
+            dp[i][1] = (fromRhombus + fromNotRhombus) % MOD;
         }
 
-        return (dp1[n - 1] + dp2[n - 1]) % MOD;
+        return (int)((dp[n][0] + dp[n][1]) % MOD);
     }
 }
